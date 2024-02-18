8759902803ab16356ef0f19f44122c61f9ef8ca2
yogyy <yogimr261@gmail.com>
2024-02-18 21:26:22 +0700

    feat: login api and route

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,50 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connect } from "@/db/config";
+import User from "@/models/usermodel";
+import jwt from "jsonwebtoken";
+import bcryipt from "bcryptjs";
+import { getErrorMessage } from "@/lib/utils";
+
+connect();
+
+export async function POST(req: NextRequest) {
+  try {
+    const reqBody = await req.json();
+    const { email, password } = reqBody;
+    console.log(reqBody);
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+    console.log("user exist");
+    const validPW = await bcryipt.compare(password, user.password);
+    if (!validPW) {
+      return NextResponse.json({ error: "Invalid password" }, { status: 401 });
+    }
+
+    const token_data = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+
+    const acc_token = await jwt.sign(token_data, process.env.SECRET_TOKEN!, {
+      expiresIn: "1h",
+    });
+
+    const res = NextResponse.json(
+      { message: "Login successfully" },
+      { status: 200 }
+    );
+    res.cookies.set("token", acc_token, {
+      httpOnly: true,
+      path: "/",
+    });
+
+    return res;
+  } catch (err) {
+    const error = err as Error;
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
