3e932a900a143c497f025d5d5a0059424bc1589f
yogyy <yogimr261@gmail.com>
2024-02-18 21:26:44 +0700

    feat: logout api

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const res = NextResponse.json({
+      message: "Logout successfully",
+      success: true,
+    });
+    res.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
+    return res;
+  } catch (err) {
+    const error = err as Error;
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
