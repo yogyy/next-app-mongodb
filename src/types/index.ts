c73f245f55079491b1102223f54116f20346a236
yogyy <yogimr261@gmail.com>
2024-05-08 22:45:59 +0700

    feat: add user response

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,15 @@
 type Token = { id: string; username: string; email: string };
 
-export type { Token };
+interface UserResponse {
+  _id: string;
+  username: string;
+  email: string;
+  password: string; // Consider using a more secure representation for password storage
+  isVerified: boolean;
+  isAdmin: boolean;
+  __v: number; // Versioning field (optional)
+  forgotPasswordToken?: string; // Optional field for handling forgot password functionality
+  forgotPasswordTokenExpiry?: Date; // Optional field for handling forgot password functionality
+}
+
+export type { Token, UserResponse };
